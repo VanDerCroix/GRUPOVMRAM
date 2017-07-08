4ec49fc7ee66f8fa06e08ca24797615bff92e3a0
Jose_Rojas <jose1248421@gmail.com>
2017-07-08 11:25:12 -0500

    Delete create_excel_file.ps1

--- RELEASE/VMRAM/create_excel_file.ps1
+++
@@ -1,9 +0,0 @@
-$excel = New-Object -ComObject "Excel.Application"
-$wb = $excel.Workbooks.Add()
-$ws = $wb.ActiveSheet
-$excel.Visible = $False
-$wb.SaveAs($PSScriptRoot + "\" +"xltest.xlsx")
-$wb.Close()
-$excel.Quit()
-
-Remove-Item $MyINvocation.InvocationName
